bfb5d23b4e80cf2a4a8b11c5c43a2d8df8710195
apple <youhappyming@163.com>
2017-10-31 17:43:16 +0800

    第一步测试

---
+++ Cp55_test/Cp55_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cp55_test
+//
+//  Created by apple on 2017/10/31.
+//  Copyright © 2017年 Taoart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Cp55_test/Cp55_testTests/Cp55_testTests.m
@@ -0,0 +1,39 @@
+//
+//  Cp55_testTests.m
+//  Cp55_testTests
+//
+//  Created by apple on 2017/10/31.
+//  Copyright © 2017年 Taoart. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Cp55_testTests : XCTestCase
+
+@end
+
+@implementation Cp55_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
